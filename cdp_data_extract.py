d739b023d8c340c6d403c8b9c8136616dc60e905
iyerintel <deepak7ravi@gmail.com>
2015-03-09 19:20:16 -0400

    Update cdp_data_extract.py

--- cdp_data_extract.py
+++ cdp_data_extract.py
@@ -32,7 +32,7 @@ def cdp_parse(pattern, cdp_data):
     return ''
 
 
-# Technique to allow importable and executable code to coexist (will explain in class#8)
+
 if __name__ == '__main__':
     
     cdp_file = 'r1_cdp.txt'
@@ -56,31 +56,3 @@ if __name__ == '__main__':
     print
 
 
-'''
- a program that opens the 'sw1_cdp.txt' file and finds all of the remote
-hostnames, remote IP addresses, and remote platforms. output should look 
-similar to the following:
- remote_hosts: ['R1', 'R2', 'R3', 'R4', 'R5']
-          IPs: ['10.1.1.1', '10.1.1.2', '10.1.1.3', '10.1.1.4', '10.1.1.5']
-     platform: ['Cisco 881', 'Cisco 881', 'Cisco 881', 'Cisco 881', 'Cisco 881']
-'''
-
-
-f = open("sw1_cdp.txt")
-cdp_data = f.read()
-f.close()
-
-dict = {}
-
-dict['remote_hosts'] = re.findall(r"Device ID: (.+)", cdp_data)
-dict['IPs'] = re.findall(r"IP address: (.+)", cdp_data)
-dict['platform'] = re.findall(r"Platform: (.+?),", cdp_data)
-
-
-# Print output
-print 
-ordered_var = ('remote_hosts', 'IPs', 'platform')
-for k in ordered_var:
-    print "%15s: %-20s" % (k, dict[k])
-
-print 
